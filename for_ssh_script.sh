12d825927177a07a640d58e7fbc7e525b83a95e7
Rafael Santos <rafa@fainf-3932.fundacred.local>
2019-12-05 17:10:39 -0300

    add for script

---
+++ for_ssh_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+USERNAME=ansadm
+PASSWORD=anspasswd
+HOSTS="host1 host2 host3"
+SCRIPT="hostname; pwd"
+for HOSTNAME in ${HOSTS} ; do
+    sshpass -p ${PASSWORD} ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"    
+done
