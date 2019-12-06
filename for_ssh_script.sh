bf8d6736e2a0dd370d07f1155b42b651301bd820
Rafael Santos <49077225+rafaelsantosxp@users.noreply.github.com>
2019-12-06 08:48:37 -0300

    Update for_ssh_script.sh

--- for_ssh_script.sh
+++ for_ssh_script.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 USERNAME=ansadm
 PASSWORD=anspasswd
-HOSTS="host1 host2 host3"
+HOSTS="
+host1
+host2
+host3
+"
 SCRIPT="hostname; pwd"
 for HOSTNAME in ${HOSTS} ; do
-    sshpass -p ${PASSWORD} ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"    
+    sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
 done
