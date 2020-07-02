f651fca7a1d3b5029968b7a096a352e4a239e88b
Rafael Santos <49077225+rafaelsantosxp@users.noreply.github.com>
2020-07-02 03:02:19 -0300

    Create tag.sh

---
+++ tag.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+LAST_COMMIT=`git show $(git tag -l | sort -V | tail --lines=1) | grep -oh 'hotfix\|feature\|release'`
+LAST_TAG=`git tag -l | sort -V | tail --lines=1`
+IFS='.' read -a vers <<< "$LAST_TAG"
+
+MAJ=${vers[0]}
+MIN=${vers[1]}
+BUG=${vers[2]}
+
+if [ $LAST_COMMIT == hotfix ]
+then
+    echo "o ultimo commit veio de um $LAST_COMMIT"
+    echo "$LAST_TAG"
+    ((BUG+=1))
+    echo "Minor Version incrementada"
+elif [ $LAST_COMMIT == feature ]
+then
+    echo "o ultimo commit veio de um $LAST_COMMIT"
+    echo "$LAST_TAG"
+    ((MIN+=1))
+    BUG=0
+    echo "Minor Version incrementada"
+elif [ $LAST_COMMIT == release ]
+then
+    echo "o ultimo commit veio de um $LAST_COMMIT"
+    echo "$LAST_TAG"
+    ((MAJ+=1))
+	MIN=0
+    BUG=0
+    echo "Major Version incrementada"
+else
+    echo "nao foi possivel verificar o ultimo commmit na Master!"
+fi
+
+NEWTAG="$MAJ.$MIN.$BUG"
+echo "adicionando Tag: $NEWTAG";
+git tag -a $NEWTAG -m $NEWTAG
