1d7b03169f4f5a4700ee217001148f66a5770a73
Rafael Lopes Santos <rafa@warmachine.dev.local>
2020-09-30 22:16:35 -0300

    eks config

--- eks_kubeconf.sh
+++ eks_kubeconf.sh
@@ -1,6 +1,6 @@
 https://aws.amazon.com/premiumsupport/knowledge-center/eks-api-server-unauthorized-error/
 
-aws eks --region us-east-1 update-kubeconfig --name test
+aws eks --region us-east-1 update-kubeconfig --name test --role-arn arn:aws:iam::0000000000:role/AdminTest
 
 aws sts get-caller-identity
 kubectl edit configmap aws-auth -n kube-system
