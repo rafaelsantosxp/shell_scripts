6f935f5b9fc457ba5541bf380f97b1045f3977f5
Rafael Santos <49077225+rafaelsantosxp@users.noreply.github.com>
2021-09-22 14:35:32 -0300

    Update tag.sh

--- tag.sh
+++ tag.sh
@@ -12,7 +12,7 @@ then
     echo "o ultimo commit veio de um $LAST_COMMIT"
     echo "$LAST_TAG"
     ((BUG+=1))
-    echo "Minor Version incrementada"
+    echo "Bug Version incrementada"
 elif [[ $LAST_COMMIT == feature ]]
 then
     echo "o ultimo commit veio de um $LAST_COMMIT"
@@ -30,6 +30,27 @@ then
     echo "Major Version incrementada"
 else
     echo "nao foi possivel verificar o ultimo commmit na Master!"
+fi
+
+if [[ -z "$MAJ" ]]
+then
+    MAJ=0
+else
+    echo "Tag MAJOR encontrada"
+fi
+
+if [[ -z "$MIN" ]]
+then
+    MIN=0
+else
+    echo "Tag MINOR encontrada"
+fi
+
+if [[ -z "$BUG" ]]
+then
+    BUG=first_tag
+else
+    echo "Tag BUG encontrada"
 fi
 
 NEWTAG="$MAJ.$MIN.$BUG"
